163ff5fcfe9a954c0927ab39ff0313f66e00007d
Andrew Stoddart <andystod@users.noreply.github.com>
2024-02-14 10:27:05 -0500

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Andrew Stoddart on 14/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
